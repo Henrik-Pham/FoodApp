dd2a03908e76f2213fa0bf6e709e8331912e3edb
Henrik-Pham <HenrikPHPham@gmail.com>
2025-07-10 22:44:03 +0200

    Adding Login and Register DTO

---
+++ HPFoods_API/Models/Dto/LoginRequestDTO.cs
@@ -0,0 +1,7 @@
+namespace HPFoods_API.Models.Dto;
+
+public class LoginRequestDto
+{
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
